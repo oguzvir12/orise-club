3ba495d477d34409b4fce7003bea8fbf978b675b
oguzvir12 <oguzvir12@gmail.com>
2026-09-07 21:35:18 +0300

    Create route.ts

---
+++ app/auth/callback/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from 'next/server'
+import { supabase } from '@/lib/supabase'
+
+export async function GET(request: Request) {
+  const requestUrl = new URL(request.url)
+  const code = requestUrl.searchParams.get('code')
+  const token_hash = requestUrl.searchParams.get('token_hash')
+  const type = requestUrl.searchParams.get('type')
+
+  if (code) {
+    await supabase.auth.exchangeCodeForSession(code)
+  } else if (token_hash && type) {
+    await supabase.auth.verifyOtp({ token_hash, type: type as any })
+  }
+
+  return NextResponse.redirect(new URL('/reset-password', requestUrl.origin))
+}
